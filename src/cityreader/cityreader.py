5773ff1108c4cdef347a5b5f36c6df0c16af8d9c
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-06 12:11:55 -0500

    cityreader.py mvp done

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,33 +1,43 @@
-# Create a class to hold a city location. Call the class "City". It should have
-# fields for name, lat and lon (representing latitude and longitude).
+import csv
+# # Create a class to hold a city location. Call the class "City". It should have
+# # fields for name, lat and lon (representing latitude and longitude)
+# # 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = (lat)
+    self.lon = (lon)
+  def __repr__(self):
+    return f'city: {self.name} | lat: {self.lat} | long: {self.lon}'
 
 
-# We have a collection of US cities with population over 750,000 stored in the
-# file "cities.csv". (CSV stands for "comma-separated values".)
-#
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
-# to read this file so that each record is imported into a City instance. Then
-# return the list with all the City instances from the function.
-# Google "python 3 csv" for references and use your Google-fu for other examples.
-#
-# Store the instances in the "cities" list, below.
-#
-# Note that the first line of the CSV is header that describes the fields--this
-# should not be loaded into a City object.
-cities = []
+# # We have a collection of US cities with population over 750,000 stored in the
+# # file "cities.csv". (CSV stands for "comma-separated values".)
+# #
+# # In the body of the `cityreader` function, use Python's built-in "csv" module 
+# # to read this file so that each record is imported into a City instance. Then
+# # return the list with all the City instances from the function.
+# # Google "python 3 csv" for references and use your Google-fu for other examples.
+# #
+# # Store the instances in the "cities" list, below.
+# #
+# # Note that the first line of the CSV is header that describes the fields--this
+# # should not be loaded into a City object.
 
-def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
-    return cities
+def cityreader():
+  cities = []
+  with open('cities.csv', "r") as data:## haveing "rb" throws encoding error
+    ## " r " = read only mode, " rb " = Opens the file as read-only in binary format 
+    reader = csv.reader(data)
+    for row in reader:
+      cities.append(City(row[0],row[3],row[4]))
+  return cities
 
-cityreader(cities)
+cities = cityreader()
 
-# Print the list of cities (name, lat, lon), 1 record per line.
+# # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
 
 # STRETCH GOAL!
 #
